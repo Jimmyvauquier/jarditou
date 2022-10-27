f89bfafc3cfd0142c08574df7512f7e4e3f65fbc
Jimmyvauquier <jimmyvauquier@yahoo.fr>
2022-10-27 16:45:24 +0200

    exo8 du 27/10

---
+++ assets/js/exo_js/test.js
@@ -0,0 +1,50 @@
+//exercice8 (exo3)
+var nbre = parseInt(prompt("saisir un nombre"));
+var resultat=nbre+nbre;
+
+while (nbre > 0) 
+{   
+    // Exécute le calcul et stocke le résultat   
+    // dans une variable nommée ‘resultat’  
+    resultat = nbre;
+
+    // A chaque tour, on affiche le résultat courant à l’utilisateur
+    console.log("resultat");
+
+    // A chaque tour, on ajoute +1 à la variable i  
+    nbre++; 
+    continue;
+
+    if (prompt==0)
+
+    {
+        break;
+    } 
+}
+
+var total=parseInt("nbre"+"nbre");
+
+console.log(total);
+console.log(moyenne);
+console.log(resultat);
+alert=moyenne;
+alert=resultat;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
