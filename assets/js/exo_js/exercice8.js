2520ae09ad21bc2d5c2b2ed07fc0d1b6883b5dde
Jimmyvauquier <jimmyvauquier@yahoo.fr>
2022-10-31 11:33:31 +0100

    travail du 31/10 js exo 8

--- assets/js/exo_js/exercice8.js
+++ assets/js/exo_js/exercice8.js
@@ -35,3 +35,72 @@ while(bool)
     }
 }
 alert("Vous avez rentré : "+nombre+"prenom");
+
+
+//exercice8 (xo2)
+var resultat = 0;
+var x = parseInt(prompt("saisir un nombre"));
+
+while(!x==resultat)
+{
+console.log (x=x-1;)
+document.write(x);
+x--;
+
+}
+
+alert=resultat;
+
+//exercice8 (exo3)
+var somme=0;
+var saisie="";
+var moyenne=parseFloat(saisie)/somme;
+ 
+ 
+while(saisie!="0"){
+  saisie=prompt("Entrez votre nombre\n Entrez 0 pour terminer");
+  somme=somme+parseFloat(saisie);
+}
+ 
+console.log("La somme maximal vaut  " + " " + (Math.max(saisie)) );
+console.log("La somme minimal vaut  " + " " + (Math.min(saisie)) );
+
+alert= (somme);
+
+//exercice8 (exo4)
+var n = parseInt(prompt("5"));
+var x= parseInt(prompt("7"));
+var multiplicateur=0;
+
+console.log("Table de multiplication par 5");
+console.log("Table de multiplication par 7");
+console.log("5 * 0 = 0");
+console.log("5 * 1 = 5");
+console.log("5 * 2 = 10");
+console.log("5 * 3 = 15");
+console.log("5 * 4 = 20");
+console.log("5 * 5 = 25") ;
+console.log("5 * 6 = 30");
+console.log("5 * 7 = 35");
+console.log("5 * 8 = 40");
+console.log("5 * 9 = 45");
+console.log("5 * 10 = 50");
+
+console.log("7 * 0 = 0");
+console.log("7 * 1 = 7");
+console.log("7 * 2 = 14");
+console.log("7 * 3 = 21");
+console.log("7 * 4 = 28");
+console.log("7 * 5 = 35") ;
+console.log("7 * 6 = 42");
+console.log("7 * 7 = 49");
+console.log("7 * 8 = 56");
+console.log("7 * 9 = 63");
+console.log("7 * 10 = 70");
+
+resultat=(x)||(n)+("*")+("multiplicateur");
+alert=resultat;
+
+
+
+
--- assets/js/exo_js/test.js
+++ assets/js/exo_js/test.js
@@ -1,42 +1,36 @@
-//exercice8 (exo3)
-var nbre = parseInt(prompt("saisir un nombre"));
-var resultat=nbre+nbre;
+//exercice8 (exo4)
+var n = parseInt(prompt("5"));
+var x= parseInt(prompt("7"));
+var multiplicateur=0;
 
-while (nbre > 0) 
-{   
-    // Exécute le calcul et stocke le résultat   
-    // dans une variable nommée ‘resultat’  
-    resultat = nbre;
+console.log("Table de multiplication par 5");
+console.log("Table de multiplication par 7");
+console.log("5 * 0 = 0");
+console.log("5 * 1 = 5");
+console.log("5 * 2 = 10");
+console.log("5 * 3 = 15");
+console.log("5 * 4 = 20");
+console.log("5 * 5 = 25") ;
+console.log("5 * 6 = 30");
+console.log("5 * 7 = 35");
+console.log("5 * 8 = 40");
+console.log("5 * 9 = 45");
+console.log("5 * 10 = 50");
 
-    // A chaque tour, on affiche le résultat courant à l’utilisateur
-    console.log("resultat");
+console.log("7 * 0 = 0");
+console.log("7 * 1 = 7");
+console.log("7 * 2 = 14");
+console.log("7 * 3 = 21");
+console.log("7 * 4 = 28");
+console.log("7 * 5 = 35") ;
+console.log("7 * 6 = 42");
+console.log("7 * 7 = 49");
+console.log("7 * 8 = 56");
+console.log("7 * 9 = 63");
+console.log("7 * 10 = 70");
 
-    // A chaque tour, on ajoute +1 à la variable i  
-    nbre++; 
-    continue;
-
-    if (prompt==0)
-
-    {
-        break;
-    } 
-}
-
-var total=parseInt("nbre"+"nbre");
-
-console.log(total);
-console.log(moyenne);
-console.log(resultat);
-alert=moyenne;
+resultat=(x)||(n)+("*")+("multiplicateur");
 alert=resultat;
-
-
-
-
-
-
-
-
 
 
 
