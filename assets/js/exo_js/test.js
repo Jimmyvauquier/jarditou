2ef8ba875b5ab5dacb3b10b23975104bdff56f3f
Jimmyvauquier <jimmyvauquier@yahoo.fr>
2022-10-31 13:53:41 +0100

    exo 8.5 du 31/10

--- assets/js/exo_js/test.js
+++ assets/js/exo_js/test.js
@@ -1,18 +1,5 @@
-//exercice8 (exo5)
-var mot = window.prompt("Saisissez votre mot");
-var c = consonnes;
-var v = voyelles;
+//exercice9
 
- voyelle = 0, consonne = 0;
-for(i=0;chaine[i];++i)
-   if( strchr("aeiouyAEIOUY", chaine[i]) )
-      ++voyelle;
-   else if( isalpha(chaine[i] ))
-    ++consonne;
-
-
-alert(consonnes);
-alert(voyelles);
 
 
 
