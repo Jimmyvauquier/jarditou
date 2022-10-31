6cccd62bf69229de2be8cc7266af9e95b619f2f2
Jimmyvauquier <jimmyvauquier@yahoo.fr>
2022-10-31 13:20:43 +0100

    exo 8.5 du 31/10

--- assets/js/exo_js/test.js
+++ assets/js/exo_js/test.js
@@ -1,41 +1,18 @@
-//exercice8 (exo4)
-var n = parseInt(prompt("5"));
-var x= parseInt(prompt("7"));
-var multiplicateur=0;
+//exercice8 (exo5)
+var mot = window.prompt("Saisissez votre mot");
+var c = consonnes;
+var v = voyelles;
 
-console.log("Table de multiplication par 5");
-console.log("Table de multiplication par 7");
-console.log("5 * 0 = 0");
-console.log("5 * 1 = 5");
-console.log("5 * 2 = 10");
-console.log("5 * 3 = 15");
-console.log("5 * 4 = 20");
-console.log("5 * 5 = 25") ;
-console.log("5 * 6 = 30");
-console.log("5 * 7 = 35");
-console.log("5 * 8 = 40");
-console.log("5 * 9 = 45");
-console.log("5 * 10 = 50");
-
-console.log("7 * 0 = 0");
-console.log("7 * 1 = 7");
-console.log("7 * 2 = 14");
-console.log("7 * 3 = 21");
-console.log("7 * 4 = 28");
-console.log("7 * 5 = 35") ;
-console.log("7 * 6 = 42");
-console.log("7 * 7 = 49");
-console.log("7 * 8 = 56");
-console.log("7 * 9 = 63");
-console.log("7 * 10 = 70");
-
-resultat=(x)||(n)+("*")+("multiplicateur");
-alert=resultat;
+ voyelle = 0, consonne = 0;
+for(i=0;chaine[i];++i)
+   if( strchr("aeiouyAEIOUY", chaine[i]) )
+      ++voyelle;
+   else if( isalpha(chaine[i] ))
+    ++consonne;
 
 
-
-
-
+alert(consonnes);
+alert(voyelles);
 
 
 
