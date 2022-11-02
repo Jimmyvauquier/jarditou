bdbf5be0bab47fbc38664671d4ca891f7388b02b
Jimmyvauquier <jimmyvauquier@yahoo.fr>
2022-11-02 16:44:51 +0100

    evalexo1

---
+++ assets/js/exo_js/eval_1.js
@@ -0,0 +1,33 @@
+var a = parseInt(prompt("entrez votre age"));
+var j = a= <20;
+var m = a= >=20;
+var v = a=>41;
+
+
+while (a>=100)
+{
+var a = parseInt(prompt("entrez votre age"));
+console.log(age);
+
+    if (a<20)
+    {
+        console.log (j=j+1);
+        j++;
+    }
+
+    if (a>=20)
+    {
+        console.log (m=m+1);
+        m++;
+    }
+
+    if (a>41)
+    {
+        console.log (v=v+1);
+        v++;
+    }
+}
+console.log(j);
+console.log(m);
+console.log(v);
+
