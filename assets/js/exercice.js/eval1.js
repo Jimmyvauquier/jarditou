b9770415c87f6d09001ad7510d04dc9a376eac14
Jimmyvauquier <jimmyvauquier@yahoo.fr>
2022-11-03 10:31:06 +0100

    exo du 0311

---
+++ assets/js/exercice.js/eval1.js
@@ -0,0 +1,29 @@
+
+var a=0;
+var j=0;
+var m=0;
+var v=0;
+var nbre=0;
+while (nbre<=100)
+{
+var nbre = parseInt(prompt("entrez votre age"));
+console.log(nbre);
+    if (nbre<20)
+    {
+        console.log (j);
+        j++;
+    }
+    if (nbre>=20 & nbre<=40)
+    {
+        console.log (m);;
+        m++;
+    }
+    if (nbre>=41)
+    {
+        console.log (v);
+        v++;
+    }
+   
+}
+let t=j+m+v;
+alert(" il y a "+ t + " personnes " + " dont "+ j + " de - de 20 ans " + m + " adultes de 20 à 40 ans " + v + " de personnes de plus de 40 ans dont 1 centenaire sans dents" );
