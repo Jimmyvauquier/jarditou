89edf255fc53d46df93b71f101cc31269d15e24e
Jimmyvauquier <jimmyvauquier@yahoo.fr>
2022-11-03 11:09:43 +0100

    eval exercice 2

--- assets/js/exercice.js/eval2.js
+++ assets/js/exercice.js/eval2.js
@@ -1,3 +1,27 @@
+var a=7;
+var b=0;
+var resultat;
+console.log("Table de multiplication par 7");
+for (b=0; b<=10; b++)
+{
+    var b = parseInt(prompt("Saisir un nombre"));
+    resultat = a * b;
+    alert (resultat);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
