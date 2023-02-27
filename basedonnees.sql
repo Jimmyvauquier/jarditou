b5f154648af4c9b9baf4f3ea0c69c68a813888ac
Jimmyvauquier <jimmyvauquier@yahoo.fr>
2023-02-27 11:47:16 +0100

    exo bis

--- basedonnees.sql
+++ basedonnees.sql
@@ -10,23 +10,23 @@ CREATE TABLE client(
         cli_adresse      VARCHAR(50),
         cli_tel          int(10)
               
-);
+       );
 CREATE TABLE commande(
         com_num          INT NOT NULL
         cli_num          INT NOT NULL,PRIMARY KEY AUTO_INCREMENT,
-        com_date         VARCHAR(50) NOT NULL,
-        com_obs          VARCHAR(50)
+        com_date         VARCHAR(50),
+        com_obs          VARCHAR(50),
       FOREIGN key (cli_num) REFERENCES client (cli_num)
-);
+       );
 CREATE TABLE produit(
-        pro_num         INT NOT NULL,PRIMARY key AUTO_INCREMENT
-        pro_libelle         INT NOT NULL,
-        pro_description      DATE NOT NULL,
-        FOREIGN KEY (pro_num) REFERENCES detail (pro_num),
+        pro_num           INT NOT NULL,PRIMARY key AUTO_INCREMENT
+        pro_libelle       INT NOT NULL,
+        pro_description   DATE NOT NULL,
+        FOREIGN KEY (pro_num) REFERENCES detail (pro_num)
        );
 CREATE TABLE detail(
         com_num          INT NOT NULL,PRIMARY key AUTO_INCREMENT
         pro_num          INT NOT NULL,
         det_qte          DATE NOT NULL,
-        FOREIGN KEY (com_num) REFERENCES commande(com_num),
-        );
+        FOREIGN KEY (com_num) REFERENCES commande(com_num)
+);
